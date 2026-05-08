a3e347e6fb623b2032b8254aa17856edd74e2d00
Paulo Henrique Ortolan <portolan@mercadona.com>
2026-05-08 11:42:11 +0100

    Initial commit

---
+++ src/main/java/pt/pauloortolan/springairagexample/SpringAiRagExampleApplication.java
@@ -0,0 +1,13 @@
+package pt.pauloortolan.springairagexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiRagExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiRagExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pt/pauloortolan/springairagexample/SpringAiRagExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.pauloortolan.springairagexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiRagExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
