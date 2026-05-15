69ef5bb23cf88312433c1fad313ad9945fd8f321
Paulo Henrique Ortolan <portolan@mercadona.com>
2026-05-15 15:15:36 +0100

    some refactorings

--- src/main/java/pt/pauloortolan/springairagexample/controllers/PopulationController.java
+++ src/main/java/pt/pauloortolan/springairagexample/controllers/PopulationController.java
@@ -3,6 +3,7 @@ package pt.pauloortolan.springairagexample.controllers;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pt.pauloortolan.springairagexample.pojo.PopulationCountResponse;
@@ -19,6 +20,11 @@ public class PopulationController {
     @GetMapping("/topTenMostPopulated")
     public PopulationCountResponse getMostPopulatedCountries() {
         return populationService.getMostPopultatedCountries();
+    }
+
+    @GetMapping("/topFiveMostPopulatedInContinent/{continent}")
+    public PopulationCountResponse getTopFivePopulatedCountriesByContinent(@PathVariable String continent) {
+        return populationService.getTopFivePopulatedCountriesByContinent(continent);
     }
 
 }
--- src/main/java/pt/pauloortolan/springairagexample/services/PopulationService.java
+++ src/main/java/pt/pauloortolan/springairagexample/services/PopulationService.java
@@ -3,9 +3,12 @@ package pt.pauloortolan.springairagexample.services;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.stereotype.Service;
 import pt.pauloortolan.springairagexample.pojo.PopulationCountResponse;
+
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -15,7 +18,13 @@ public class PopulationService {
     private final ChatClient raggedChatClient;
 
     private static final PromptTemplate TOP_TEN_POPULATED_COUNTRIES_PROMPT = new PromptTemplate("""
-            Extract the top 10 populated countries from the provided context documents.
+            Extract the top ten (10) most populated countries from the provided Population context documents.
+            For each country found, provide its rank, name, and 2022 population.
+            Return all countries found in the context, ordered by rank."
+            """);
+
+    private static final PromptTemplate TOP_FIVE_POPULATED_COUNTRIES_PROMPT = new PromptTemplate("""
+            Extract the top five (5) most populated countries from the provided Population context documents located in the continent {continent}.
             For each country found, provide its rank, name, and 2022 population.
             Return all countries found in the context, ordered by rank."
             """);
@@ -26,6 +35,18 @@ public class PopulationService {
 
         return raggedChatClient
                 .prompt(TOP_TEN_POPULATED_COUNTRIES_PROMPT.create())
+                .call()
+                .entity(PopulationCountResponse.class);
+    }
+
+    public PopulationCountResponse getTopFivePopulatedCountriesByContinent(String continent) {
+        log.info("PopulationService::getTopFivePopulatedCountriesByContinent(continent={})", continent);
+
+        Prompt prompt = TOP_FIVE_POPULATED_COUNTRIES_PROMPT
+                .create(Map.of("continent", continent));
+
+        return raggedChatClient
+                .prompt(prompt)
                 .call()
                 .entity(PopulationCountResponse.class);
     }
